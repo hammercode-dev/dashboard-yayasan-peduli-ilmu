636ca8f6ba801b0e397b4a419393905b365626e2
mpsalunggg <putrasatria893@gmail.com>
2026-02-18 08:13:27 +0700

    chore: add button back

--- src/features/program/pages/CreateProgramPage.tsx
+++ src/features/program/pages/CreateProgramPage.tsx
@@ -1,8 +1,17 @@
+import Link from "next/link"
+import { Button } from "@/components/ui/button"
+import { ArrowLeft } from "lucide-react"
 import ProgramDonationForm from "../components/ProgramDonationForm"
 
 export default function CreateProgramPage() {
   return (
-    <section className="space-y-6">
+    <section className="space-y-4">
+      <Link href="/dashboard/program">
+        <Button variant="link" className="p-0! mb-2 cursor-pointer">
+          <ArrowLeft className="size-4" />
+          Kembali
+        </Button>
+      </Link>
       <div className="flex flex-col">
         <h1 className="text-2xl font-bold tracking-tight">
           Tambah Program Donasi
